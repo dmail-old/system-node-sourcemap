bc2cf1736275c663767e3763cb2aca977e20b996
Damien Maillard <dmaillard06@gmail.com>
2016-03-18 19:12:56 +0100

    eslint

--- index.js
+++ index.js
@@ -1,91 +1,95 @@
 /* global System */
 
-if( typeof System === 'undefined' ){
-	throw new Error('System is undefined, you must require es6-module-loader or Systemjs before this one');
+if (typeof System === 'undefined') {
+    throw new Error('System is undefined, you must require es6-module-loader or Systemjs before this one');
 }
 
 var sourceMap = require('node-sourcemap');
 
-function readSource(path){
-	path = path.replace('!transpiled', '');
-	//path = System.normalize(path);
+function readSource(path) {
+    path = path.replace('!transpiled', '');
+    // path = System.normalize(path);
 
-	if( false === path in System.sources ){
-		//throw new Error('source undefined for ' + path);
-	}
-	else{
-		return System.sources[path];
-	}
+    if ((path in System.sources) === false) {
+        // throw new Error('source undefined for ' + path);
+    } else {
+        return System.sources[path];
+    }
 }
 
-if( !System.sources ){
-	System.sources = {};
+if (!System.sources) {
+    System.sources = {};
 
-	var improveSyntaxError = function(error){
-		if( error && error.name === 'SyntaxError' && error._babel ){
-			// error.loc contains {line: 0, column: 0}
-			var match = error.message.match(/([\s\S]+): Unterminated string constant \(([0-9]+)\:([0-9]+)/);
+    var improveSyntaxError = function(error) {
+        if (error && error.name === 'SyntaxError' && error._babel) {
+            // error.loc contains {line: 0, column: 0}
+            var match = error.message.match(/([\s\S]+): Unterminated string constant \(([0-9]+)\:([0-9]+)/);
 
-			if( match ){
-				var improvedError = new SyntaxError();
-				var column = match[3];
-				column+= 63; // because node-sourcemap/index.js:155 will do column-=63
+            if (match) {
+                var improvedError = new SyntaxError();
+                var column = match[3];
+                column += 63; // because node-sourcemap/index.js:155 will do column-=63
 
-				improvedError.stack = 'SyntaxError: Unterminated string constant\n\t at ' + match[1] + ':' + match[2] + ':' + column;
+                var stack = '';
 
-				return improvedError;
-			}
-		}
+                stack += 'SyntaxError: Unterminated string constant\n\t at ';
+                stack += match[1] + ':' + match[2] + ':' + column;
 
-		return error;
-	};
+                improvedError.stack = stack;
 
-	var translateMethod = System.translate;
-	System.translate = function(load){
-		return translateMethod.call(this, load).then(function(source){
-			System.sources[load.address] = source;
-			return source;
-		}).catch(function(error){
-			error = improveSyntaxError(error);
-			return Promise.reject(error);
-		});
-	};
+                return improvedError;
+            }
+        }
 
-	System.trace = true;
+        return error;
+    };
 
-	// error are sourcemapped before being logged
-	/*
-	Object.defineProperty(Error.prototype, 'inspect', {
-		enumerable: false,
-		configurable: true,
-		value: function(){
-			return sourceMap.transformError(this, readSource).toString();
-		}
-	});
-	*/
+    var translateMethod = System.translate;
+    System.translate = function(load) {
+        return translateMethod.call(this, load).then(function(source) {
+            System.sources[load.address] = source;
+            return source;
+        }).catch(function(error) {
+            error = improveSyntaxError(error);
+            return Promise.reject(error);
+        });
+    };
 
-	/*
-	// not needed anymore thanks to Error.prepareStackTrace
+    System.trace = true;
 
-	var importMethod = System.import;
-	System.import = function(){
-		return importMethod.apply(this, arguments).catch(function(error){
-			error = sourceMap.transformError(error, readSource);
-			return Promise.reject(error);
-		});
-	};
+    // error are sourcemapped before being logged
+    /*
+    Object.defineProperty(Error.prototype, 'inspect', {
+        enumerable: false,
+        configurable: true,
+        value: function(){
+            return sourceMap.transformError(this, readSource).toString();
+        }
+    });
+    */
 
-	process.on('uncaughtException', function handleUncaughtException(error){
-		sourceMap.transformError(error, readSource);
-		throw error;
-	});
-	*/
+    /*
+    // not needed anymore thanks to Error.prepareStackTrace
+
+    var importMethod = System.import;
+    System.import = function(){
+        return importMethod.apply(this, arguments).catch(function(error){
+            error = sourceMap.transformError(error, readSource);
+            return Promise.reject(error);
+        });
+    };
+
+    process.on('uncaughtException', function handleUncaughtException(error){
+        sourceMap.transformError(error, readSource);
+        throw error;
+    });
+    */
 }
 
 module.exports = {
-	StackTrace: sourceMap.StackTrace,
+    StackTrace: sourceMap.StackTrace,
 
-	install: function(){
-		sourceMap.install(readSource);
-	}
+    install: function() {
+        sourceMap.install(readSource);
+    }
 };
